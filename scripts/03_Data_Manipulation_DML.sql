b53daad0da2c017b842abe13f319c07c22ee582c
KHOO ZI WEI <khoo2602@gmail.com>
2025-09-09 17:40:42 +0800

    Update 03_Data_Manipulation_DML.sql

--- scripts/03_Data_Manipulation_DML.sql
+++ scripts/03_Data_Manipulation_DML.sql
@@ -81,7 +81,7 @@ FROM customers
 WHERE score IS NULL
 
 /* ============================================================================== 
-   DELETE & TRUNCATE (DELETE can use WHERE as it checks the data row by row (more performance), but TRUNCATE cant use WHERE as it will delete the whole table)
+   DELETE & TRUNCATE (DELETE can use WHERE as it checks the data row by row (slow and more performance), but TRUNCATE cant use WHERE as it will delete the whole table)
 =============================================================================== */
 
 -- Select customers with an ID greater than 5 before deleting
@@ -98,4 +98,5 @@ DELETE FROM persons
 
 -- Faster method to delete all rows, especially useful for large tables
 TRUNCATE TABLE persons
+
 
