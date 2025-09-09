599e86011a09ebdaa583a68b67098f763518a3cd
KHOO ZI WEI <khoo2602@gmail.com>
2025-09-09 17:05:43 +0800

    Update 01_Query_Data_SELECT.sql

--- scripts/01_Query_Data_SELECT.sql
+++ scripts/01_Query_Data_SELECT.sql
@@ -210,14 +210,10 @@ FROM orders
 ORDER BY order_date DESC
 
 /* ==============================================================================
-   All Together
+   All Together (Coding Order)
 =============================================================================== */
 
-/* Calculate the average score for each country 
-   considering only customers with a score not equal to 0
-   and return only those countries with an average score greater than 430
-   and sort the results by the highest average score first. */
-SELECT
+SELECT DISTINCT TOP 2
     country,
     AVG(score) AS avg_score
 FROM customers
@@ -247,6 +243,7 @@ SELECT
     'New Customer' AS customer_type
 
 FROM customers;
+
 
 
 
